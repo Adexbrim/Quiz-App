6d010b724fab1709a8d1c5538dd080966f298e3f
Adeola <adexplus90@gmail.com>
2024-12-27 19:27:09 +0100

    Pushing to git

---
+++ src/components/data.js
@@ -0,0 +1,132 @@
+const data = [
+    {
+        id : 1,
+        question : 'Rolex is a company that specializes in what type of product?',
+        answer : [
+            {
+                text : "phone",
+                correct : false
+            },
+            {
+                text : "watches",
+                correct : true
+            },
+            {
+                text : "food",
+                correct : false
+            },
+            {
+                text : "cosmetics",
+                correct : false
+            },
+
+        ]
+    },
+
+    {
+        id : 2,
+        question : 'When did the website facebook launch?',
+        answer : [
+            {
+                text : "2004",
+                correct : true
+            },
+            {
+                text : "2005",
+                correct : false
+            },
+            {
+                text : "2006",
+                correct : false
+            },
+            {
+                text : "2007",
+                correct : false
+            },
+
+        ]
+    },
+
+    {
+        id : 3,
+        question : "Which is the best software company in Nigeria?",
+        answer : [
+            {
+                text : "Facebook",
+                correct : false
+            },
+
+            {
+                text : "Nigeria Coder's Academy",
+                correct : false
+            },
+
+            {
+                text : "Virtual Flux Integrated",
+                correct : true
+            },
+
+            {
+                text : "Ogtech Network's Ltd",
+                correct : true
+            },
+
+        ]
+    },
+
+    {
+        id : 4,
+        question : "Who played the character of Harry Potter in movie?",
+        answer : [
+
+            {
+                text : "Ed Sheeran",
+                correct : false
+            },
+
+            {
+                text : "Hugh Jackman",
+                correct : false
+            },
+
+            {
+                text : "Daniel RadCliff",
+                correct : true
+            },
+
+            {
+                text : "Denzel Washington",
+                correct : true
+            },
+        ]
+    },
+
+    {
+        id : 5,
+        question : 'Who is the first black president of America',
+        answer : [
+            {
+                text : "Goodluck Jonathan",
+                correct : false
+            },
+
+            {
+                text : "Barack Obama",
+                correct : true
+            },
+
+            {
+                text : "Ibrahim Adeola",
+                correct : false
+            },
+
+            {
+                text : "Hillary Cliton",
+                correct : false
+            },
+
+        ]
+    },
+]
+
+export default data;
